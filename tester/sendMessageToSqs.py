c9219ee5b19667a706a09889328609e89cfcbaf5
DavidJGGm <jonathangmz79@hotmail.com>
2024-03-02 18:56:53 -0600

    tester modified

--- tester/sendMessageToSqs.py
+++ tester/sendMessageToSqs.py
@@ -7,10 +7,10 @@ sqs = boto3.client('sqs')
 
 def sendMessage(msg, index):
     for i in range(0, index):
-        response = sqs.send_message(MessageBody='El mensaje: ' + msg + " --- " + str(i), QueueUrl='https://sqs.us-east-1.amazonaws.com/192552324558/sqs-handler-my-stack-02032024')
-        print(response)
+        sqs.send_message(MessageBody='El mensaje: ' + msg + " --- subIndex: " + str(i), QueueUrl='https://sqs.us-east-1.amazonaws.com/192552324558/sqs-handler-my-stack-02032024')
+        print("[OK] "+i)
     
 
-for i in range(10000):
-    hilo = threading.Thread(target=sendMessage, args=("Hola", 10000))
+for i in range(100):
+    hilo = threading.Thread(target=sendMessage, args=("Hola hilo principal: "+i, i))
     hilo.start()
